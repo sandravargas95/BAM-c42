ffa2dc470ae43aa4d0273c99af4da0934b21df51
sandravargas95 <sandymv95@gmail.com>
2022-11-13 17:25:54 -0300

    agregar

---
+++ san-app/src/componentes/componente1.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ const componente1 = () => {
+  return (
+    <div>
+      <h1> hola jonas</h1>
+      <p> lalalalalalallaal</p>
+    </div>
+  )
+}
+
+export default componente1
+
